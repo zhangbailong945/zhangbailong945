378273b212b507ea26d8c461310e0dbe876375b2
Administrator <Administrator@192.168.244.129>
2017-01-17 13:31:45 +0800

    update zhangbailong945

---
+++ application/logs/log-2017-01-16.php
@@ -0,0 +1,187 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2017-01-16 16:42:40 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 16:42:40 --> No URI present. Default controller set.
+DEBUG - 2017-01-16 16:42:40 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 16:42:40 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 16:42:44 --> cURL Class Initialized
+DEBUG - 2017-01-16 16:42:44 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 16:42:45 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 16:42:45 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 16:42:46 --> cURL Class Initialized
+DEBUG - 2017-01-16 16:42:46 --> Common class already loaded. Second attempt ignored.
+ERROR - 2017-01-16 16:42:46 --> Query error: Table 'loachblog.articles' doesn't exist - Invalid query: SELECT * FROM `articles` WHERE 1=1 order by article_id desc limit 0,5
+ERROR - 2017-01-16 16:42:46 --> Query error: Table 'loachblog.articles' doesn't exist - Invalid query: SELECT COUNT(*) AS `numrows` FROM `articles`
+ERROR - 2017-01-16 16:42:46 --> Severity: Error --> Call to a member function num_rows() on a non-object D:\zhangbailong945\system\database\DB_driver.php 1144
+DEBUG - 2017-01-16 16:43:52 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 16:43:52 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 16:43:52 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 16:43:53 --> cURL Class Initialized
+DEBUG - 2017-01-16 16:43:53 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 16:43:54 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 16:43:55 --> Total execution time: 2.7622
+DEBUG - 2017-01-16 16:43:55 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 16:43:55 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-16 16:43:55 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-16 16:44:03 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 16:44:03 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 16:44:03 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 16:44:04 --> cURL Class Initialized
+DEBUG - 2017-01-16 16:44:04 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 16:44:04 --> Total execution time: 1.3181
+DEBUG - 2017-01-16 16:44:11 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 16:44:11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 16:44:11 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 16:44:12 --> cURL Class Initialized
+DEBUG - 2017-01-16 16:44:12 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 16:44:12 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 16:44:12 --> Total execution time: 1.2291
+DEBUG - 2017-01-16 16:44:12 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 16:44:12 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-16 16:44:12 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-16 16:44:12 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 16:44:12 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 16:44:12 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 16:44:14 --> cURL Class Initialized
+DEBUG - 2017-01-16 16:44:14 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 16:44:14 --> Total execution time: 1.2291
+DEBUG - 2017-01-16 17:29:44 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:29:44 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:29:44 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:29:46 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:29:46 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:29:46 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:29:46 --> Total execution time: 2.0921
+DEBUG - 2017-01-16 17:29:46 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:29:46 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-16 17:29:46 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-16 17:29:47 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:29:47 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:29:47 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:29:48 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:29:48 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:29:48 --> Total execution time: 1.1831
+DEBUG - 2017-01-16 17:29:52 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:29:52 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:29:52 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:29:53 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:29:53 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:29:54 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:29:54 --> Total execution time: 1.4341
+DEBUG - 2017-01-16 17:29:54 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:29:54 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-16 17:29:54 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-16 17:29:57 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:29:57 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:29:57 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:29:58 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:29:58 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:29:58 --> Total execution time: 1.1561
+DEBUG - 2017-01-16 17:30:32 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:30:32 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:30:32 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:30:33 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:30:34 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:30:34 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:30:34 --> Total execution time: 1.2581
+DEBUG - 2017-01-16 17:30:34 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:30:34 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-16 17:30:34 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-16 17:30:34 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:30:34 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:30:34 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:30:35 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:30:35 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:30:35 --> Total execution time: 1.2431
+DEBUG - 2017-01-16 17:35:29 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:35:29 --> No URI present. Default controller set.
+DEBUG - 2017-01-16 17:35:29 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:35:29 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:35:30 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:35:30 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:35:30 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:35:30 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:35:31 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:35:31 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:35:32 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:35:32 --> Total execution time: 1.2851
+DEBUG - 2017-01-16 17:35:32 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:35:32 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-16 17:35:32 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-16 17:35:40 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:35:40 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:35:40 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:35:41 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:35:41 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:35:41 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:35:41 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:35:41 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:35:41 --> Total execution time: 1.2581
+DEBUG - 2017-01-16 17:35:42 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:35:42 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:35:55 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:35:55 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:35:55 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:35:56 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:35:56 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:36:07 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:36:07 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:36:07 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:36:08 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:36:08 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:36:08 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:36:08 --> Total execution time: 1.1851
+DEBUG - 2017-01-16 17:36:08 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:36:08 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-16 17:36:08 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-16 17:36:08 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:36:08 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:36:08 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:36:10 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:36:10 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:36:10 --> Total execution time: 1.2771
+DEBUG - 2017-01-16 17:36:13 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:36:13 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:36:13 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:36:14 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:36:14 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:36:14 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:36:14 --> Total execution time: 1.2681
+DEBUG - 2017-01-16 17:36:14 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:36:14 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:36:14 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:36:16 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:36:16 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:36:16 --> Total execution time: 1.1891
+DEBUG - 2017-01-16 17:37:06 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:37:06 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:37:06 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:37:07 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:37:07 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:37:07 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:37:07 --> Total execution time: 1.2081
+DEBUG - 2017-01-16 17:37:07 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:37:07 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-16 17:37:07 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-16 17:37:07 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:37:07 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:37:07 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:37:09 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:37:09 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:37:09 --> Total execution time: 1.2751
+DEBUG - 2017-01-16 17:37:13 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:37:13 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:37:13 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:37:14 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:37:14 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:37:14 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:37:14 --> Total execution time: 1.2171
+DEBUG - 2017-01-16 17:37:15 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:37:15 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-16 17:37:15 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-16 17:37:15 --> UTF-8 Support Enabled
+DEBUG - 2017-01-16 17:37:15 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-16 17:37:15 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-16 17:37:16 --> cURL Class Initialized
+DEBUG - 2017-01-16 17:37:16 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-16 17:37:16 --> Total execution time: 1.2121
---
+++ application/logs/log-2017-01-17.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2017-01-17 11:46:06 --> UTF-8 Support Enabled
+DEBUG - 2017-01-17 11:46:06 --> No URI present. Default controller set.
+DEBUG - 2017-01-17 11:46:06 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-17 11:46:06 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-17 11:46:08 --> cURL Class Initialized
+DEBUG - 2017-01-17 11:46:08 --> UTF-8 Support Enabled
+DEBUG - 2017-01-17 11:46:08 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-17 11:46:08 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-17 11:46:09 --> cURL Class Initialized
+DEBUG - 2017-01-17 11:46:09 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-17 11:46:11 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-17 11:46:11 --> Total execution time: 2.9625
+DEBUG - 2017-01-17 11:46:12 --> UTF-8 Support Enabled
+DEBUG - 2017-01-17 11:46:12 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-17 11:46:12 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-17 11:46:20 --> UTF-8 Support Enabled
+DEBUG - 2017-01-17 11:46:20 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-17 11:46:20 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-17 11:46:22 --> cURL Class Initialized
+DEBUG - 2017-01-17 11:46:22 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-17 11:46:22 --> Total execution time: 1.2825
+DEBUG - 2017-01-17 11:51:50 --> UTF-8 Support Enabled
+DEBUG - 2017-01-17 11:51:50 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-17 11:51:50 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-17 11:51:51 --> cURL Class Initialized
+DEBUG - 2017-01-17 11:51:51 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-17 11:51:51 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-17 11:51:51 --> Total execution time: 1.3350
+DEBUG - 2017-01-17 11:51:52 --> UTF-8 Support Enabled
+DEBUG - 2017-01-17 11:51:52 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-17 11:51:52 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-17 11:51:52 --> UTF-8 Support Enabled
+DEBUG - 2017-01-17 11:51:52 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-17 11:51:52 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-17 11:51:53 --> cURL Class Initialized
+DEBUG - 2017-01-17 11:51:53 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-17 11:51:53 --> Total execution time: 1.1892
+DEBUG - 2017-01-17 11:53:08 --> UTF-8 Support Enabled
+DEBUG - 2017-01-17 11:53:09 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-17 11:53:09 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-17 11:53:10 --> cURL Class Initialized
+DEBUG - 2017-01-17 11:53:10 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-17 11:53:10 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-17 11:53:10 --> Total execution time: 1.2500
+DEBUG - 2017-01-17 11:53:10 --> UTF-8 Support Enabled
+DEBUG - 2017-01-17 11:53:10 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2017-01-17 11:53:10 --> 404 Page Not Found: Communal/LoachBlog
+DEBUG - 2017-01-17 11:53:10 --> UTF-8 Support Enabled
+DEBUG - 2017-01-17 11:53:10 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2017-01-17 11:53:10 --> Config file loaded: D:\zhangbailong945\application\config/page_config.php
+DEBUG - 2017-01-17 11:53:11 --> cURL Class Initialized
+DEBUG - 2017-01-17 11:53:11 --> Common class already loaded. Second attempt ignored.
+DEBUG - 2017-01-17 11:53:11 --> Total execution time: 1.1604
